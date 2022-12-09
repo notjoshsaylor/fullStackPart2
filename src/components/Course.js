de4f9d2f8c1b793394b71571ac044f4ac72b0e05
Joshua Saylor <joshuasaylor01@gmail.com>
2022-12-09 10:45:02 -0500

    full stack open ex. 2.2

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,16 +1,41 @@
-
-const Course = ({course}) =>{
-  console.log(course)
-  const parts = course.parts.map(part =>
-    <li key={part.id}>{part.name} -- {part.exercises}</li> 
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
   )
-    console.log(parts)
-    return(
-      <div>
-        <h1>{course.name}  </h1>
-        <h2>{parts}</h2>  
-      </div>
+}
+
+const Header = ({name}) => {
+  return <h1>{name}</h1>
+}
+
+const Content = ({parts}) => {
+  return(
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      )
     )
   }
+    </div>
+  )
+}
 
+const Part = ({name, exercises}) => {
+  return(
+    <h2>
+      {name} {exercises}
+    </h2>
+  )
+}
+
+const Total = ({parts}) => {
+  const total = parts.reduce((sum, part) => {
+    return sum + part.exercises
+  }, 0)
+  return <h3>Total of {total} exercises</h3>
+}
 export default Course
